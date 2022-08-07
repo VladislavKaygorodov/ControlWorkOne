9027f0d3ce23e0cc81efef7d5faffcfe46c03ce3
Vladislav <kaygorodov.vlad@gmail.com>
2022-08-07 23:11:12 +0700

    Инициализация метода выборки нужных слов

--- Program.cs
+++ Program.cs
@@ -1,20 +1,48 @@
-﻿void FindStringOnLetterSize(string[] array, int num)
+﻿/*void FindStringOnLetterSize(string[] array, int num)
 {
-
     int collFindString = 0;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length == num) collFindString++;
     }
     Console.WriteLine(collFindString);
+    CreateArray(collFindString);
 }
 
+void CreateArray(int size)
+{
+    string[] array = new string[size];
+    Console.WriteLine($"[{String.Join("+", array)}]");
+}
 
-string[] array = new string[] { "color", "one", "table", "moskow", "two", "four" };
-Console.WriteLine($"[{String.Join(",", array)}]");
+void FillArray(string[] firstArray, string[] secondArray)
+{
+
+}*/
+
+string[] startArray = new string[] { "12", "color", "one", "9", "table", "moskow", "two", "four", "set", "1", "h", "lesson" };
+Console.WriteLine($"[{String.Join(",", startArray)}]");
 Console.WriteLine("Слова из скольки букв нужно выбрать из массива? ");
 int findLetter = Convert.ToInt32(Console.ReadLine());
 
+int collFindString = 0;
 
-FindStringOnLetterSize(array, findLetter);
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length == findLetter) collFindString++;
+}
 
+string[] endArray = new string[collFindString];
+
+int endArrayIndex = 0;
+
+for (int j = 0; j < startArray.Length; j++)
+{
+    if (startArray[j].Length == findLetter)
+    {
+        endArray[endArrayIndex] = startArray[j];
+        endArrayIndex++;
+    }
+}
+
+Console.WriteLine($"[{String.Join("+", endArray)}]");
