dd123a61fba602f0b505307987e0c6b85f370c59
Vladislav <kaygorodov.vlad@gmail.com>
2022-08-07 22:44:09 +0700

    Выполнен способ поиска колличества искомых слов в массиве

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿string[] array = new string[] { "color", "one", "table", "moskow", "two", "four"};
+﻿void FindStringOnLetterSize(string[] array, int num)
+{
+
+    int collFindString = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length == num) collFindString++;
+    }
+    Console.WriteLine(collFindString);
+}
+
+
+string[] array = new string[] { "color", "one", "table", "moskow", "two", "four" };
 Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine("Слова из скольки букв нужно выбрать из массива? ");
+int findLetter = Convert.ToInt32(Console.ReadLine());
+
+
+FindStringOnLetterSize(array, findLetter);
+
